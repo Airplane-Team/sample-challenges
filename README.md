1117567d7b2a079c78666f7562ebcbc9d05596f1
Alexander List <atlex2@gmail.com>
2024-06-13 23:53:24 -0400

    initial sample challenges commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Sample Shirley Challenges by Airplane.Team
+
+You can always get the latest version of this repository by visiting [github.com/airplane-team/sample-challenges](github.com/airplane-team/sample-challenges).
+
+If you need help, you can join us on our [Discord server](https://airplane.team/discord).
+
+## Typed Challenges
+
+To access features like multi-line strings, build your challenge in typescript and run it with `ts-node`.
+
+```bash
+ts-node typescript/stol.ts
+```
+
+### Dependency on `ts-node`
+
+But first you need to install `ts-node`. Usually you'll want to do it globally, so you can run it from any directory. To do so, run the following command in your terminal:
+
+```bash
+npm install -g ts-node
+```
+
+You can also install it locally:
+
+```bash
+npm install ts-node
+```
+
+Note that when you install it locally, you'll need to run it with `npx`:
+
+```bash
+npx ts-node typescript/stol.ts
+```
+
+### Dependency on `npm` and `node.js`
+
+If you don't have `npm` installed, you can install it with [node.js](https://nodejs.org/).
---
+++ specification/challenge.ts
@@ -0,0 +1,68 @@
+export type AircraftType = 'da42' | 'c172p' | 'a250vtol' | 'ask21' | 'pa18';
+
+// The enabled tools are the tools that are available to the user
+// Empty object / undefined tools indicates no preference compared to default
+export interface EnabledTools {
+  weatherLookup?: boolean;
+  muteAndStandby?: boolean;
+  airportCodeLookup?: boolean;
+  pohLookup?: boolean;
+  checklists?: boolean;
+  pilotNotes?: boolean;
+  // resets the call context
+  resetContext?: boolean;
+  // lets the user deliver feedback to the Challenge maker and Airplane.team
+  feedback?: boolean;
+  // sets a startpoint for the user that then they can get distance from
+  // via `distanceFromStartpoint` enabled state
+  setUserWaypoint?: boolean;
+}
+
+export interface EnabledState {
+  // when not empty, the state will navigate to these waypoints
+  navigateToStateWaypoints?: {
+    latitudeDegrees: number;
+    longitudeDegrees: number;
+    altitudeMSL: number;
+    name: string;
+  }[];
+  // when set true, the state will navigate to the user waypoint
+  navigateToUserWaypoint?: boolean;
+  // when set true, the state will get the distance from the user waypoint
+  distanceFromUserWaypoint?: boolean;
+}
+
+export interface PhaseTransition {
+  toPhaseName: string;
+  conditions?: string;
+  automaticTransition?: boolean;
+}
+
+export interface Phase {
+  name: string;
+  prompt: string;
+  shortDescription?: string;
+  transitions: PhaseTransition[];
+  // override the enabled tools for this phase.
+  // When option set here and on challenge level, the phase level takes precedence.
+  enabledTools?: EnabledTools;
+  // override the enabled state options for this phase.
+  // When option set here and on challenge level, the phase level takes precedence.
+  enabledState?: EnabledState;
+}
+
+export interface Challenge {
+  name: string;
+  shortDescription: string;
+  prompt: string;
+  // if null, all tools are disabled unless overridden by phase
+  // if undefined or empty object, state is enabled by Shirley default (currently all off)
+  enabledTools?: EnabledTools | null;
+  // if null, state is disabled unless overridden by phase
+  // if undefined or empty object, state is enabled by Shirley default (currently all off)
+  enabledState?: EnabledState | null;
+  // if specified, the challenge is only available for the specified aircraft
+  limitToAircraft?: AircraftType[];
+  // the first phase is the initial phase
+  phases: Phase[];
+}
