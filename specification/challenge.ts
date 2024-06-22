43154ae14ac4fec12a158e2c528826076e09a92b
Alexander Hoekje List <atlex2@gmail.com>
2024-06-21 23:05:25 -0700

    Air-169 adds more complex stol challenge; updates specification (#1)
    * adds startUserTimer
    
    * adds user timeers
    
    * navigateToStateWaypoints temporarily commented-out
    
    * moves to make room for new stol challenge
    
    * Adds advanced stol challenge used at FSExpo
    
    * simple-stol output json path

--- specification/challenge.ts
+++ specification/challenge.ts
@@ -16,16 +16,19 @@ export interface EnabledTools {
   // sets a startpoint for the user that then they can get distance from
   // via `distanceFromStartpoint` enabled state
   setUserWaypoint?: boolean;
+  // starts / restarts a timer that the user can check the elapsed time
+  startUserTimer?: boolean;
 }
 
 export interface EnabledState {
-  // when not empty, the state will navigate to these waypoints
-  navigateToStateWaypoints?: {
-    latitudeDegrees: number;
-    longitudeDegrees: number;
-    altitudeMSL: number;
-    name: string;
-  }[];
+  // COMING SOON
+  // // when not empty, the state will navigate to these waypoints
+  // navigateToStateWaypoints?: {
+  //   latitudeDegrees: number;
+  //   longitudeDegrees: number;
+  //   altitudeMSL: number;
+  //   name: string;
+  // }[];
   // when set true, the state will navigate to the user waypoint
   navigateToUserWaypoint?: boolean;
   // when set true, the state will get the distance from the user waypoint
