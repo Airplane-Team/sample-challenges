1ce4873323fdbee5ac81af35ab0d9c5f6957e026
Alexander List <atlex2@gmail.com>
2025-02-01 13:42:34 -0500

    updates specification and adds zod

--- specification/waypoint.ts
+++ specification/waypoint.ts
@@ -1,11 +1,27 @@
-/** Waypoint is a location with optional altitude and name. */
-export type Waypoint = {
+import { z } from 'zod';
+
+/** Type describing the kind of waypoint. */
+export enum WaypointType {
+  /** Default. Waypoint is a location with optional altitude and name. */
+  Generic = 'Generic',
+  /** Waypoint is an airport. */
+  Airport = 'Airport',
+}
+
+export const WaypointSchema = z.object({
   /** Latitude in degrees. */
-  latitudeDeg: number;
+  latitudeDeg: z.number().min(-90).max(90),
   /** Longitude in degrees. */
-  longitudeDeg: number;
+  longitudeDeg: z.number().min(-180).max(180),
   /** Optional altitude in feet MSL. */
-  mslAltitudeFt?: number;
+  mslAltitudeFt: z.number().optional(),
   /** Optional name of the waypoint. */
-  name?: string;
-};
+  name: z.string().optional(),
+  /** Type of waypoint. */
+  type: z.nativeEnum(WaypointType).optional(),
+  /** Desired magnetic heading upon arrival at the waypoint. */
+  magneticHeadingDeg: z.number().min(0).max(360).optional(),
+});
+
+/** Waypoint is a location with optional altitude and name. */
+export type Waypoint = z.infer<typeof WaypointSchema>;
